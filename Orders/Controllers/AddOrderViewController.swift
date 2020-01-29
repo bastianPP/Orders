7dce11ed96e6055b01c9f04158f6ccbe7c5a1849
SEBASTIAN GONZALEZ <bastian_pp@hotmail.com>
2020-01-29 11:08:45 -0600

    Se crean la pantalla de ordenes y la pantalla para agregar ordenes en el Storyboard, así como las controladores (clases) de las pantallas.

---
+++ Orders/Controllers/AddOrderViewController.swift
@@ -0,0 +1,14 @@
+//
+//  AddOrderViewController.swift
+//  Orders
+//
+//  Created by Pedro Paramo on 1/29/20.
+//  Copyright © 2020 Pedro Paramo. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AddOrderViewControlloer: UIViewController {
+    
+}
---
+++ Orders/Controllers/OrdersTableViewController.swift
@@ -0,0 +1,14 @@
+//
+//  OrdersTableViewController.swift
+//  Orders
+//
+//  Created by Pedro Paramo on 1/29/20.
+//  Copyright © 2020 Pedro Paramo. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class OrdersTableViewController: UITableViewController {
+    
+}
